58ddb9e419cb32c1b7f13d90907cd46c2f6dabb1
Ankit Pandey <ankitpandey090256@gmail.com>
2025-12-18 09:53:15 +0545

    quote

--- Userfinder/src/App.jsx
+++ Userfinder/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Userfinder from './components/Userfinder'
+import QuotesGen from './components/QuotesGen'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Userfinder />
+      <QuotesGen />
     </>
   )
 }
---
+++ Userfinder/src/components/QuotesGen.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "./quotes.css";
+
+const QuotesGen = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchQuote = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://dummyjson.com/quotes/random");
+      const data = await response.json();
+      setQuote(data.quote);console.log(data);
+      setAuthor(data.author);
+    } catch (error) {
+      setQuote("Failed to load quote");
+      setAuthor("");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+
+  return (
+    <div className="container">
+      <h1>Quotes Generator 💬</h1>
+
+      <div className="card">
+        <p className="quote">{loading ? "Loading..." : `"${quote}"`}</p>
+        {!loading && <p className="author">— {author}</p>}
+
+        <div className="buttons">
+          <button onClick={fetchQuote}>New Quote</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuotesGen;
